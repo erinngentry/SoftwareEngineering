e165bab9323422f33c4430f01d4b38dd1091de90
Erinn Gentry <erinn.gentry@bison.howard.edu>
2019-09-18 16:14:27 +0000

    Margin Problem

--- Dat1.py
+++ Dat1.py
@@ -1,122 +1,100 @@
-Margin Problem
-
-
+# Margin Problem
+# Erinn Gentry
 
 # User enters file name with .txt extension
-
-print("Enter file with .txt extension: ")
-
+print('Enter file with .txt extension: ')
 user_file = input()
 
-
-
 # User enters custom # of inches for left margin
-
-print("Enter left margin size (in inches): ")
-
+print('Enter left margin size (in inches): ')
 left_inches_margin = int(input())
 
-
-
 # Display left margin in inches and convert
-
 # left_margin size from inches to pt
-
-print("Left margin size:", left_inches_margin)
-
+print('Left margin size:', left_inches_margin)
 left_margin = left_inches_margin * 12
 
-
-
 # User enters custom # of inches for right margin
-
-print("Enter right margin size (in inches): ")
-
+print('Enter right margin size (in inches): ')
 right_inches_margin = int(input())
 
-
-
 # Display right margin in inches
-
-print("Right margin size:", right_inches_margin)
-
-
+print('Right margin size:', right_inches_margin)
 
 # Total page width in pt
-
 page_width = 144
 
-
-
 # Convert right_margin size from inches to pt
-
 # and subtracting that from total page_width to 
-
 # keep aligned.
-
 right_margin = right_inches_margin * 12
 
-
-
 char = 80
-
 # Read-in file line by line
+# 'r' reads from file called sample.txt
+f = open(user_file, 'r')
 
-# "r" reads from file called sample.txt
+# Test Case 1: Check if file exists
+if f.readable() == False:
+    print('Cannot read file. Please make sure file exists.')
+    
+print('Creating new file...')
 
-f = open(user_file, "r")
+fileIn = f.read()
+words = fileIn.split(' ')
+sentences = fileIn.split('  ')
+while '' in words:
+    words.remove('')
+print(words)
+
+count = 0
+for x in sentences:
+    count += 1
 
 
 def create_left_margin():
-
     i = 0
-
     while i < left_margin:
-
-        h.write("_")
-
+        h.write(' ')
         i += 1
-
         
-
 def create_right_margin():
-
     i = page_width - right_margin
-
     while i < page_width:
-
-        h.write("_")
-
+        h.write(' ')
         i += 1
 
-space = page_width - left_margin - right_margin
+# Print file with margins
+# for x in range(0, 30, 1):
+#     if x < page_width:
+#         create_left_margin()
+#         print(f.read(char - left_margin - right_margin), sep = '', end = '')
+#         create_right_margin()
+#     print('\n', sep = '', end = '')
+#     x += 1
+
+# x = 0
+# if x < page_width:
+#     create_left_margin()
+#     print(f.read(char - left_margin - right_margin), sep = '', end = '')
+#     create_right_margin()
 
 
+h = open('output.txt', 'w+')
+for x in range(0, count, 1):
+    space = page_width - left_margin - right_margin
+    create_left_margin()
+#         line = f.read(char - left_margin - right_margin)        
+    
+    while(len(words[x]) > 0 and len(words[x]) < space and len(words) > 0):
 
-line = f.read(space)
+        h.write(words[x])
+        space = space - len(words[x])
+        del words[x]
 
-words = line.split(" ")
-
-print(words)
-
-sentences = line.split(".")
-
-print(sentences)
-
-
-
-h = open("output.txt", "w+")
-
-for x in range(0, 30, 1):
-
-    if x < page_width:
-
-        create_left_margin()
-
-        h.write(line + "\n")
-
-        create_right_margin()
-
-    x += 1
-
+        if len(words) <= 0:
+            break
+    create_right_margin()
+    h.write('\n')
+        
 
